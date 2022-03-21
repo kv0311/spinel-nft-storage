92c72797de852ef4541cad32946be479d2f173a1
kv0311 <kvxi98@gmail.com>
2022-03-21 15:13:33 +0700

    init storage nft

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use('/img',express.static('SpinelNFT'))
+app.use('/metadata',express.static('SpinelMetadata'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
